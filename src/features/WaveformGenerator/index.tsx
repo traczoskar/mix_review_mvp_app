dee2451f507207c8058e7dc1f27a4c29cc9e9525
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-25 18:22:42 +0100

    updated WaveformGenerator with Redux state management functions

--- src/features/WaveformGenerator/index.tsx
+++ src/features/WaveformGenerator/index.tsx
@@ -1,42 +1,43 @@
-import { useEffect, useRef, useState } from "react";
+import {
+  setFileUploaded,
+  selectFileUploaded,
+  selectIsFileUploaded,
+} from "../UploadFile/uploadFileSlice";
+import { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import WaveSurfer from "wavesurfer.js";
 import { PlayerContainer } from "./styled";
 
 export const WaveformGenerator = () => {
-  const [fileUploaded, setFileUploaded] = useState("");
+  const fileUploaded = useSelector(selectFileUploaded);
+  const isFileUploaded = useSelector(selectIsFileUploaded);
   const waveformRef = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch();
   let wavesurfer: WaveSurfer;
-
-  // Function to handle the file upload
-  const handleUpload = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const file = (e.target as HTMLFormElement).file.files[0] as File;
-    const fileUrl = URL.createObjectURL(file);
-    setFileUploaded(fileUrl as any);
-    console.log("File uploaded and stored:", fileUrl);
-  };
 
   // Function (useEffect) to create the waveform
   useEffect(() => {
-    wavesurfer = WaveSurfer.create({
-      container: waveformRef.current!,
-      waveColor: "#ffffff40",
-      progressColor: "#a200ff",
-      url: fileUploaded,
-      dragToSeek: true,
-      width: "35vw",
-      height: 60,
-      hideScrollbar: false,
-      barGap: 1,
-      barHeight: 10,
-      barRadius: 10,
-      barWidth: 1.5,
-      normalize: true,
-    });
-    return () => {
-      wavesurfer.destroy();
-    };
-  }, [fileUploaded]);
+    if (isFileUploaded && fileUploaded) {
+      wavesurfer = WaveSurfer.create({
+        container: waveformRef.current!,
+        waveColor: "#ffffff40",
+        progressColor: "#a200ff",
+        url: fileUploaded,
+        dragToSeek: true,
+        width: "35vw",
+        height: 60,
+        hideScrollbar: false,
+        barGap: 1,
+        barHeight: 10,
+        barRadius: 10,
+        barWidth: 1.5,
+        normalize: true,
+      });
+      return () => {
+        wavesurfer.destroy();
+      };
+    }
+  }, [isFileUploaded, fileUploaded]);
 
   const handleStop = () => {
     if (wavesurfer) {
@@ -60,9 +61,20 @@ export const WaveformGenerator = () => {
     }
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const fileInput = e.currentTarget.file as HTMLInputElement;
+    const file = fileInput.files ? fileInput.files[0] : null;
+    if (file) {
+      const fileUrl = URL.createObjectURL(file);
+      dispatch(setFileUploaded(fileUrl));
+      console.log("File uploaded and stored:", fileUrl);
+    }
+  };
+
   return (
     <>
-      <form onSubmit={handleUpload}>
+      <form onSubmit={handleSubmit}>
         <input type="file" name="file" />
         <button type="submit">Upload</button>
       </form>
