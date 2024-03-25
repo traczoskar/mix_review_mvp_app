c3cbde72b036977198d07118669a43a4b128239d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-25 16:22:15 +0100

    added HomePage index.tsx file

---
+++ src/views/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import { WaveformGenerator } from "../../features/WaveformGenerator";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the HomePage!</h1>
+      <WaveformGenerator />
+    </div>
+  );
+};
+
+export default HomePage;
