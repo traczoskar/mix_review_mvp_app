7bf156387045b85afb843c063ee385f31d463b90
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-25 18:23:11 +0100

    added store Provider to main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
+import { Provider } from "react-redux";
+import store from "./App/store";
 import { GlobalStyle } from "./GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
