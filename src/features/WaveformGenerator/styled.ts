7175d7ac5d10a379f46c1d07135c6f4016a30aac
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-26 22:14:34 +0100

    added styling for FileInfoWindow

--- src/features/WaveformGenerator/styled.ts
+++ src/features/WaveformGenerator/styled.ts
@@ -29,9 +29,30 @@ export const FormContainer = styled.form`
   margin: 50px 0;
 `;
 
+export const FileInfoWindow = styled.div`
+  margin: 30px 0;
+  font-size: 16px;
+  color: #dddddd;
+  font-family: monospace;
+`;
+
 export const CurrentTimeWindow = styled.div`
   margin: 30px 0;
   font-size: 25px;
   color: #dddddd;
   font-family: monospace;
+`;
+
+export const Button = styled.button`
+  border: none;
+  border-radius: 20px;
+  font-size: 16px;
+  padding: 10px 20px;
+  color: black;
+  background-color: #dddddd;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #bbbbbb;
+  }
 `;
