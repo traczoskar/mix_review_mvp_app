109f973bc3d901de6556f5ffff69f1ed394bc976
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-26 14:47:09 +0100

    updated styles for WaveformGenerator component

--- src/features/WaveformGenerator/styled.ts
+++ src/features/WaveformGenerator/styled.ts
@@ -1,7 +1,37 @@
 import styled from "styled-components";
 
+export const PlayerSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const PlayerContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const ControlsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin: 50px 0;
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin: 50px 0;
+`;
+
+export const CurrentTimeWindow = styled.div`
+  margin: 30px 0;
+  font-size: 25px;
+  color: #dddddd;
+  font-family: monospace;
 `;
